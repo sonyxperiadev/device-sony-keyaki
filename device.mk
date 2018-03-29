3ef0f6e5cd4e717c805b8ac749b4ed0f2192eca5
Alin Jerpelea <alin.jerpelea@sony.com>
2018-03-29 15:18:50 +0200

    PACKAGES: remove fingerprint HAL
    the fingerprint HAL has been migrated to the HIDL style
    and we can remove the old package
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- device.mk
+++ device.mk
@@ -75,10 +75,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     power.keyaki
 
-# Fingerprint HAL
-PRODUCT_PACKAGES += \
-    fingerprint.keyaki
-
 # NFC config
 PRODUCT_PACKAGES += \
     nfc_nci.keyaki
